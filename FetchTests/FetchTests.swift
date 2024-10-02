504f64821b669d98e27cb2ed075ca3a93ac52e1f
Tomas Gonzalez <tsgonzalez01@gmail.com>
2024-10-02 11:19:11 -0400

    Initial Commit

---
+++ FetchTests/FetchTests.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTests.swift
+//  FetchTests
+//
+//  Created by Tomas Gonzalez on 10/2/24.
+//
+
+import Testing
+@testable import Fetch
+
+struct FetchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
